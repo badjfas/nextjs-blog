347cdf9ad0390cd50b70045eb398c48b42a4b6d8
배진우 <bjwkor@naver.com>
2021-01-31 00:42:36 +0900

    material-ui example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
-const Home = () => {
-  return <div> Hello </div>;
-};
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-export default Home;
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
